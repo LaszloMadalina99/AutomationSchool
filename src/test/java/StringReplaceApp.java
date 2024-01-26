e677003adfccd1033f3bf9ef74bff613f0efdbe9
madalina.laszlo <madalinadiana728@yahoo.com>
2024-01-26 16:21:28 +0100

    Add second Task3

---
+++ src/test/java/StringReplaceApp.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class StringReplaceApp {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input first string: ");
+        String firstString = scanner.nextLine();
+        System.out.println("Input which part do you want to change");
+        String changePart = scanner.nextLine();
+        System.out.println("Input the new text: ");
+        String lastString = scanner.nextLine();
+
+        /*
+         String strReplaced = firstString.replace(changePart, lastString);
+        System.out.println("New String is: " +  strReplaced);
+         */
+        int numberLetter = firstString.indexOf(changePart);
+        if(numberLetter>-1) {
+            String finalString = firstString.substring(0, numberLetter);
+            System.out.println("Final string is: " + finalString + lastString);
+        }
+        else{
+            System.out.println("The words are not find");
+        }
+    }
+}
+
