dd077a33d17b03f6a2b30802e6b38d1347dbd80a
madalina.laszlo <madalinadiana728@yahoo.com>
2024-01-29 14:26:59 +0100

    Add second Task4

--- src/test/java/ReverseArray.java
+++ src/test/java/ReverseArray.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class ReverseArray {
     public static void main(String[] args) {
-        int[] numbList = new int[10];
+        int[] numList = new int[10];
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter 8 numbers: ");
 
         for (int i = 0; i < 8; i++) {
-            numbList[i] = scanner.nextInt();
+            numList[i] = scanner.nextInt();
 
         }
         System.out.println("Reverse order is: ");
         scanner.close();
-        for (int i = numbList.length - 1; i >= 0; i--) {
-            System.out.println( numbList[i]);
+        for (int i = numList.length - 1; i >= 0; i--) {
+            System.out.println( numList[i]);
         }
     }
 }
