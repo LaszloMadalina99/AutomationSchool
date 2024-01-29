bee7df6cbabbe31b98972c7a91153f51a3d3eb03
madalina.laszlo <madalinadiana728@yahoo.com>
2024-01-29 14:20:12 +0100

    Add third Task4

---
+++ src/test/java/MaxArray.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class MaxArray {
+
+    public static void main(String[] args) {
+
+        int[] maxnum = new int[20];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter 20 numbers: ");
+        for (int i = 0; i < 5; i++) {
+            maxnum[i] = scanner.nextInt();
+
+        }
+        scanner.close();
+        int maxnumber = max(maxnum);
+        System.out.println("The biggest number is: " + maxnumber);
+    }
+
+    public static int max(int[] a) {
+        int biggestnumber = a[0];
+        for (int i = 1; i < 5; i++) {
+            if (a[i] > biggestnumber) {
+                biggestnumber = a[i];
+            }
+        }
+        return biggestnumber;
+    }
+}
