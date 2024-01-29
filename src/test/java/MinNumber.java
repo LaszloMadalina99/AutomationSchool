814036d171bdbbf37e825012cf2f8600c53c3671
madalina.laszlo <madalinadiana728@yahoo.com>
2024-01-29 17:40:55 +0100

    Add first Task for topic5

---
+++ src/test/java/MinNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MinNumber {
+
+    public static void main(String[] args){
+        Scanner numbers = new Scanner(System.in);
+        System.out.println("Insert first number: ");
+
+        int firstNumber = numbers.nextInt();
+
+        System.out.println("Insert second number: ");
+        int secondNumber = numbers.nextInt();
+
+        if(firstNumber>secondNumber){
+            System.out.println("The minimum number is: " + secondNumber);
+        }
+        else{
+            System.out.println("The minimum number is: " + firstNumber);
+        }
+    }
+}
