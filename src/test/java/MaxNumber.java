7e7861deaf66a7103667365fe34e68243b68593b
madalina.laszlo <madalinadiana728@yahoo.com>
2024-01-30 14:16:14 +0100

    Add second exercise for task5

---
+++ src/test/java/MaxNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MaxNumber {
+    public static void main(String[] args){
+        int[] numbers = new int[4];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert 4 numbers: ");
+        int maxNumber = numbers[0];
+        for(int i = 0; i<numbers.length; i++){
+            int addNumbers = scanner.nextInt();
+            numbers[i] = addNumbers;
+            if(numbers[i]>maxNumber){
+                maxNumber = numbers[i];
+            }
+        }
+        System.out.println("Max number is: " + maxNumber);
+        scanner.close();
+
+    }
+}
